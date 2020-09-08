4fd7dc29976133c2983e5a16cbacd87160c685f4
Taweryawer <vlad707020@gmail.com>
2020-09-08 05:49:53 -0700

    MessageBuilderBuilder

--- src/main/java/taweryawer/RestBot.java
+++ src/main/java/taweryawer/RestBot.java
@@ -1,11 +1,30 @@
 package taweryawer;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import taweryawer.service.StateMachineService;
 
 public class RestBot extends TelegramLongPollingBot {
+
+    private StateMachineService stateMachineService;
+    private Log log = LogFactory.getLog(RestBot.class);
+
+    @Autowired
+    public RestBot(StateMachineService stateMachineService) {
+        this.stateMachineService = stateMachineService;
+    }
+
     @Override
     public void onUpdateReceived(Update update) {
+        try {
+            if (update.hasMessage()) {
+            }
+        } catch (Exception e) {
+            log.error("Something went wrong", e);
+        }
 
     }
 
