679dff6e5e8cb49bb236906081c92d3de7295644
Taweryawer <vlad707020@gmail.com>
2020-09-08 05:22:02 -0700

    Autowire StateMachineRuntimePersister + spring config

--- src/main/java/taweryawer/config/StateMachineConfiguration.java
+++ src/main/java/taweryawer/config/StateMachineConfiguration.java
@@ -1,8 +1,13 @@
 package taweryawer.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.statemachine.config.EnumStateMachineConfigurerAdapter;
 import org.springframework.statemachine.config.builders.StateMachineConfigurationConfigurer;
 import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+import org.springframework.statemachine.data.jpa.JpaPersistingStateMachineInterceptor;
+import org.springframework.statemachine.data.jpa.JpaStateMachineRepository;
+import org.springframework.statemachine.persist.StateMachineRuntimePersister;
 import taweryawer.statemachine.UserEvent;
 import taweryawer.statemachine.UserState;
 
@@ -10,11 +15,17 @@ import java.util.EnumSet;
 
 public class StateMachineConfiguration extends EnumStateMachineConfigurerAdapter<UserState, UserEvent> {
 
+    @Autowired
+    private StateMachineRuntimePersister<UserState, UserEvent, String> stateMachineRuntimePersister;
+
     @Override
     public void configure(StateMachineConfigurationConfigurer<UserState, UserEvent> config) throws Exception {
         config
                 .withConfiguration()
-                    .autoStartup(false);
+                    .autoStartup(false)
+                .and()
+                    .withPersistence()
+                        .runtimePersister(stateMachineRuntimePersister);
     }
 
     @Override
@@ -23,5 +34,10 @@ public class StateMachineConfiguration extends EnumStateMachineConfigurerAdapter
                 .withStates()
                 .initial(UserState.NORMAL)
                 .states(EnumSet.allOf(UserState.class));
+    }
+
+    @Bean
+    public StateMachineRuntimePersister<UserState, UserEvent, String> stateMachineRuntimePersister(JpaStateMachineRepository jpaStateMachineRepository) {
+        return new JpaPersistingStateMachineInterceptor<>(jpaStateMachineRepository);
     }
 }
