62039679db7a9487dd32299026f108a4bb80b6ff
Taweryawer <vlad707020@gmail.com>
2020-10-01 00:05:23 -0700

    foodrepository

---
+++ src/main/java/taweryawer/repository/impl/FoodRepositoryImpl.java
@@ -0,0 +1,34 @@
+package taweryawer.repository.impl;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Repository;
+import taweryawer.entities.Food;
+import taweryawer.repository.FoodRepository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+@Repository
+public class FoodRepositoryImpl implements FoodRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    private Log log = LogFactory.getLog(FoodRepositoryImpl.class);
+
+    @Override
+    public List<Food> getFoodByCategory(String category) {
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Food> cq = cb.createQuery(Food.class);
+        Root<Food> root = cq.from(Food.class);
+        cq.select(root).where(cb.equal(root.get("category").get("name"), category));
+        TypedQuery<Food> query = entityManager.createQuery(cq);
+        return query.getResultList();
+    }
+}
